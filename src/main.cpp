83246aa253dd50b20f83892eda0375bd03f4ce9c
dennispfueller <kleine-banane@outlook.de>
2026-08-10 14:55:05 +0200

    Influx Query hinzugefügt

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,17 @@
 #include <secrets.h>
 #include <WiFi.h>
 #include <ESPAsyncWebServer.h>
+#include <HTTPClient.h>
 
 BleKeyboard bleKeyboard;
 Preferences prefs;
 AsyncWebServer server(80);
+HTTPClient http;
+
+
+const unsigned long pollIntervall = 20 * 1000;
+unsigned long lastPollTime = 0;
+String fluxQuery = "tempData = from(bucket: \"monitoring\") |> range(start: -30s) |> filter(fn: (r) => r._measurement == \"temp\") |> last() |> map(fn: (r) => ({_time: r._time, id: r.sensor, _value: r._value}))\n\ncpuData = from(bucket: \"monitoring\") |> range(start: -30s) |> filter(fn: (r) => r._measurement == \"cpu\" and r.cpu == \"cpu-total\" and r._field == \"usage_system\") |> last() |> map(fn: (r) => ({_time: r._time, id: \"cpu_usage\", _value: r._value}))\n\nunion(tables: [tempData, cpuData]) |> keep(columns: [\"_time\", \"id\", \"_value\"])";
 
 const char* NVS_KEY_KEYCODES = "keycodes";
 const int rows[3] = {4, 5, 6};
@@ -35,6 +42,18 @@ uint8_t keycodes[3][5][4] = {
     { KEY_BACKSPACE, 0, 0, 0 },                          
   },
 };
+
+struct ColumnIndices {
+  int valueIndex;
+  int sensorIndex;
+} columnIndices;
+
+struct SensorData {
+  String coretemp_package_id_0;
+  String coretemp_core_0;
+  String cpu_usage;
+  String pch_cometlake;
+} sensorData;
 
 bool lastState[3][5] = {false};
 bool lastRawState[3][5] = {false};
@@ -88,6 +107,105 @@ String buildPage() {
   return html;
 }
 
+void findValueIndex(String row) {
+  row.trim();
+  int index = 0;
+  int startPosition = 0;
+  int endPosition = 0;
+  while (endPosition != -1) {
+    endPosition = row.indexOf(",", startPosition);
+    if (row.substring(startPosition, endPosition) == "_value") {
+      columnIndices.valueIndex = index;
+    }
+    if (row.substring(startPosition, endPosition) == "id") {
+      columnIndices.sensorIndex = index;
+    }
+    index++;
+    startPosition = endPosition + 1;
+  }
+}
+
+String findValue(String row, int searchIndex) {
+  row.trim();
+  int index = 0;
+  int startPosition = 0;
+  int endPosition = 0;
+  while (endPosition != -1) {
+    endPosition = row.indexOf(",", startPosition);
+    if (index == searchIndex) {
+      if (endPosition == -1) {
+        return row.substring(startPosition);
+      }
+      return row.substring(startPosition, endPosition);
+    }
+    index++;
+    startPosition = endPosition + 1;
+  }
+  return "";
+}
+
+void evaluateString(String string) {
+  int index = 0;
+  int startPosition = 0;
+  int endPosition = 0;
+  String value;
+  String sensor;
+  while (endPosition != -1) {
+    endPosition = string.indexOf("\n", startPosition);
+    if (index == 0) {
+      if (endPosition == -1) {
+        findValueIndex(string.substring(startPosition));
+      } else {
+        findValueIndex(string.substring(startPosition, endPosition));
+      }  
+      Serial.println("SensorIndex: " + columnIndices.sensorIndex);
+      Serial.println("ValueIndex: " + columnIndices.valueIndex);
+    } else if (endPosition == -1) {
+      value = findValue(string.substring(startPosition), columnIndices.valueIndex);
+      sensor = findValue(string.substring(startPosition), columnIndices.sensorIndex);
+    } else {
+      value = findValue(string.substring(startPosition, endPosition), columnIndices.valueIndex);
+      sensor = findValue(string.substring(startPosition, endPosition), columnIndices.sensorIndex);
+    }
+    index++;
+    startPosition = endPosition + 1;
+    Serial.println("Sensor: " + sensor);
+    Serial.println("Value: " + value);
+
+    if (sensor == "coretemp_package_id_0") {
+      sensorData.coretemp_package_id_0 = value;
+    }
+    else if (sensor == "coretemp_core_0") {
+      sensorData.coretemp_core_0 = value;
+    }
+    else if (sensor == "pch_cometlake") {
+      sensorData.pch_cometlake = value;
+    }
+    else if (sensor == "cpu_usage") {
+      sensorData.cpu_usage = value;
+    }
+  }
+  Serial.println("package: " + sensorData.coretemp_package_id_0);
+  Serial.println("cometlake: " + sensorData.pch_cometlake);
+  Serial.println("usage: " + sensorData.cpu_usage);
+  Serial.println("core 0: " + sensorData.coretemp_core_0);
+}
+
+void checkKeyPress() {
+  if (bleKeyboard.isConnected()) {
+    for (int row = 0; row < 3; row++) {
+      digitalWrite(rows[row], HIGH);
+      for (int column = 0; column < 5; column++) {
+        bool currentState = digitalRead(columns[column]);
+        if (checkKeyAction(row, column, currentState)) {
+          typeKeys(currentState, row, column);
+        }
+      }
+      digitalWrite(rows[row], LOW);
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   delay(2000);
@@ -127,7 +245,7 @@ void setup() {
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/html", buildPage());
   });
-  
+
   server.on("/save", HTTP_POST, [](AsyncWebServerRequest *request) {
     for (int row = 0; row < 3; row++) {
       for (int column = 0; column < 5; column++) {
@@ -144,19 +262,23 @@ void setup() {
   });
 
   server.begin();
+
+  http.begin("http://192.168.178.73:8086/api/v2/query?org=home");
+  http.addHeader("Authorization", "Token " + String(INFLUX_TOKEN));
+  http.addHeader("Content-Type", "application/vnd.flux");
 }
 
 void loop() {
-  if (bleKeyboard.isConnected()) {
-    for (int row = 0; row < 3; row++) {
-      digitalWrite(rows[row], HIGH);
-      for (int column = 0; column < 5; column++) {
-        bool currentState = digitalRead(columns[column]);
-        if (checkKeyAction(row, column, currentState)) {
-          typeKeys(currentState, row, column);
-        }
-      }
-      digitalWrite(rows[row], LOW);
+  checkKeyPress();
+
+  if (pollIntervall < millis() - lastPollTime) {
+    int httpCode = http.POST(fluxQuery);
+    lastPollTime = millis();
+    if (httpCode == 200) {
+      String response = http.getString();
+      evaluateString(response);
+    } else {
+      // Fehler auf Display anzeigen
     }
   }
 }
